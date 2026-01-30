f09f6da1cc0a7493f481dc34efb8ed5be0cb1efc
wyrdwon <tarmac2817@proton.me>
2026-01-30 07:46:37 +0300

    Update .config/mise/config.toml Update .config/nvim/.stylua.toml Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/configs/conform.lua Update .config/nvim/lua/configs/lspconfig.lua Update .config/nvim/lua/chadrc.lua Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/configs/executable_lspconfig.lua
+++ dot_config/nvim/lua/configs/executable_lspconfig.lua
@@ -5,38 +5,24 @@ local mason_lspconfig = require "mason-lspconfig"
 
 -- Automatically enable ALL servers installed via Mason UI
 mason_lspconfig.setup {
-  -- Don't specify ensure_installed - manage via Mason UI instead
-  ensure_installed = {},
+	-- Don't specify ensure_installed - manage via Mason UI instead
+	ensure_installed = {},
 
-  -- Automatically enable any server installed through Mason
-  automatic_enable = true,
+	-- Automatically enable any server installed through Mason
+	automatic_enable = true,
 }
 
--- Setting up Pyright using vim.lsp
-vim.lsp.config.pyright = {
-  on_attach = function(client, bufnr)
-    -- Disable Pyright's formatting (Ruff handles it)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-  end,
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic", -- gives type checking errors but not linting
-        diagnosticMode = "openFilesOnly",
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-      },
-    },
-  },
-}
-
--- Setting up Ruff (linter and formatter) using vim.lsp
-vim.lsp.config.ruff = {
-  on_attach = function(client, bufnr)
-    -- Disable Ruff's completion feature, let Pyright handle it
-    client.server_capabilities.completionProvider = false
-  end,
+vim.lsp.config.basedpyright = {
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic",
+				diagnosticMode = "openFilesOnly",
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 }
 
 -- -- Configure these servers to actually be launched when needed
